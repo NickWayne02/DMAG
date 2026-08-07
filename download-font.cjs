920a6e7f5d4639d52387d3b16fa5adc8fea3dba6
Yevhenii Kostin <kostin@kdu.edu.ua>
2026-08-07 23:19:53 +0300

    Fix

--- download-font.cjs
+++ download-font.cjs
@@ -1,19 +1,29 @@
 const fs = require('fs');
 const https = require('https');
 
-const url = 'https://raw.githubusercontent.com/google/fonts/main/apache/roboto/Roboto-Regular.ttf';
-const dest = 'Roboto-Regular.ttf';
+const url = 'https://raw.githubusercontent.com/googlefonts/roboto/main/src/hinted/Roboto-Regular.ttf';
 
-https.get(url, (res) => {
-  const chunks = [];
-  res.on('data', (chunk) => chunks.push(chunk));
-  res.on('end', () => {
-    const buffer = Buffer.concat(chunks);
-    const base64 = buffer.toString('base64');
-    const tsContent = `export const ROBOTO_BASE64 = "${base64}";\n`;
-    fs.writeFileSync('src/lib/roboto-base64.ts', tsContent);
-    console.log('Successfully created roboto-base64.ts');
+function download(url) {
+  https.get(url, (res) => {
+    if (res.statusCode === 301 || res.statusCode === 302) {
+      return download(res.headers.location);
+    }
+    const chunks = [];
+    res.on('data', (chunk) => chunks.push(chunk));
+    res.on('end', () => {
+      const buffer = Buffer.concat(chunks);
+      if (buffer.length < 1000) {
+        console.error('Failed to download, buffer too small:', buffer.toString());
+        return;
+      }
+      const base64 = buffer.toString('base64');
+      const tsContent = `export const ROBOTO_BASE64 = "${base64}";\n`;
+      fs.writeFileSync('src/lib/roboto-base64.ts', tsContent);
+      console.log('Successfully created roboto-base64.ts, size:', base64.length);
+    });
+  }).on('error', (err) => {
+    console.error('Error fetching font:', err);
   });
-}).on('error', (err) => {
-  console.error('Error fetching font:', err);
-});
+}
+
+download(url);

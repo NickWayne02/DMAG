19eab946557a867237cf3d8884a633d2ab214b2d
Yevhenii Kostin <kostin@kdu.edu.ua>
2026-08-31 23:12:09 +0300

    Fix SSR window not defined error from ionic pwa elements

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -8,15 +8,15 @@ import {
   Scripts,
 } from "@tanstack/react-router";
 import { useEffect, type ReactNode } from "react";
-import { defineCustomElements } from '@ionic/pwa-elements/loader';
-
 import appCss from "../styles.css?url";
 import { Toaster } from "@/components/ui/sonner";
 import { LanguageProvider } from "@/lib/i18n";
 import { SettingsProvider } from "@/lib/settings";
 
 if (typeof window !== 'undefined') {
-  defineCustomElements(window);
+  import('@ionic/pwa-elements/loader').then(({ defineCustomElements }) => {
+    defineCustomElements(window);
+  });
 }
 
 function NotFoundComponent() {
